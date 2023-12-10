9773b305231d2511f8d122063a7eac0b7d030658
MirageM <imiragesz@gmail.com>
2023-12-10 18:16:41 -0500

    <h1> Hello, {props.firstName}</h1>

---
+++ passing-props/src/App.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName="Bob" />
+            <Heading firstName="Jack" />
+        </div>
+    );
+};
+
+export default App;
